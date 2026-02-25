c3533302278edea951f1df7d459d73a0d06e616f
22-7-co <x22han7@gmail.com>
2026-02-25 21:03:19 +0800

    Stop tracking aiops-main folder as per .gitignore

--- aiops-main/lesson/module_13/module/helm/version.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    helm = {
-      source  = "n-oden/helm"
-      version = "0.0.1"
-    }
-  }
-}
--- aiops-main/lesson/module_2/demo_5/modules/k3s/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "password" {
-  default = "password123"
-}
-
-variable "public_ip" {}
-
-variable "private_ip" {}
